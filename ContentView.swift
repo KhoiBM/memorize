3d2aa169db4a3e55a2988e73bd48638d96235cea
KhoiBM <minhkhoi.mike@gmail.com>
2020-06-25 20:44:24 +0700

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Bui Minh Khoi on 6/24/20.
+//  Copyright © 2020 Bui Minh Khoi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack(){
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .foregroundColor(Color.orange)
+//            .padding([.horizontal],100)
+            .padding()
+            .font(Font.largeTitle)
+        
+    }
+}
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:10.0).fill(Color.white)
+                        RoundedRectangle(cornerRadius:10.0).stroke(lineWidth:3)
+                        Text("👻").padding()
+            }else{
+                RoundedRectangle(cornerRadius:10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
